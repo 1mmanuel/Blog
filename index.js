f0d3bde7217a14183844444fa13b1e541dd8ac09
1mmanuel <99174608+1mmanuel@users.noreply.github.com>
2024-05-14 14:01:38 +0300

    updated the file structure

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
+const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const blogRouter = require("./controllers/blog");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = "mongodb://localhost/bloglist";
-mongoose.connect(mongoUrl);
+mongoose.set("strictQuery", false);
+logger.info("connecting to", config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then((result) => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
